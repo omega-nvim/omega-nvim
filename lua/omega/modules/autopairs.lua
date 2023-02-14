6a509b39108600fe9d52a830ddf5502557ea6b73
Max <max.heinzer397@gmx.ch>
2023-02-14 20:29:07 +0100

    feat(modules): added nvim-autopairs

---
+++ lua/omega/modules/autopairs.lua
@@ -0,0 +1,17 @@
+local autopairs = {
+    "windwp/nvim-autopairs",
+    enabled = true,
+    event = "InsertEnter",
+}
+
+autopairs.config = function()
+    local Rule = require("nvim-autopairs.rule")
+    local npairs = require("nvim-autopairs")
+    require("nvim-autopairs").setup({
+        ignored_next_char = "",
+        disable_filetype = { "norg" },
+    })
+    npairs.add_rule(Rule("$", "$", "tex"))
+end
+
+return autopairs
