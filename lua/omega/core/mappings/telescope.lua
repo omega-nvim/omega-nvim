f8bfef370f0e88badbfa39389f0f763906c2f958
Max <max.heinzer397@gmx.ch>
2023-02-16 13:59:52 +0100

    feat(mappings): lazy and other mappings added

--- lua/omega/core/mappings/telescope.lua
+++ lua/omega/core/mappings/telescope.lua
@@ -4,6 +4,10 @@ map("n", "<leader>ff", function()
     require("telescope.builtin").find_files()
 end, { desc = " Find file" })
 
+map("n", "<leader>.", function()
+    require("telescope").extensions.file_browser.file_browser()
+end, { desc = " File Browser" })
+
 map("n", "<leader>hh", function()
     require("telescope.builtin").help_tags()
 end, { desc = " Help tags" })
@@ -15,3 +19,7 @@ end, { desc = " Live Grep" })
 map("n", "<c-s>", function()
     require("telescope.builtin").current_buffer_fuzzy_find()
 end, { desc = "Current buffer fuzzy find" })
+
+map("n", "<leader>,", function()
+    require("telescope.builtin").buffers()
+end, { desc = "﩯Buffers" })
