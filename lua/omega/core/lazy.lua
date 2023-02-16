b315ecd25cab717e404040ba04cf6ab03449c191
Max <max.heinzer397@gmx.ch>
2023-02-16 22:48:49 +0100

    feat(custom): add lazy config

--- lua/omega/core/lazy.lua
+++ lua/omega/core/lazy.lua
@@ -11,57 +11,60 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
-require("lazy").setup("omega.modules", {
-    defaults = {
-        lazy = true,
-    },
-    ui = {
-        icons = {
-            lazy = "鈴",
+require("lazy").setup(
+    "omega.modules",
+    vim.tbl_deep_extend("force", {
+        defaults = {
+            lazy = true,
         },
-    },
-    diff = {
-        cmd = "git",
-    },
-    performance = {
-        rtp = {
-            paths = {
-                vim.fn.expand("~") .. "/.config/nvim",
-            },
-            disabled_plugins = {
-                "loaded_python3_provider",
-                "python_provider",
-                "node_provider",
-                "ruby_provider",
-                "perl_provider",
-                "2html_plugin",
-                "getscript",
-                "getscriptPlugin",
-                "gzip",
-                "tar",
-                "tarPlugin",
-                "rrhelper",
-                "vimball",
-                "vimballPlugin",
-                "zip",
-                "zipPlugin",
-                "tutor",
-                "rplugin",
-                "logiPat",
-                "netrwSettings",
-                "netrwFileHandlers",
-                "syntax",
-                "synmenu",
-                "optwin",
-                "compiler",
-                "bugreport",
-                "ftplugin",
-                "load_ftplugin",
-                "indent_on",
-                "netrwPlugin",
-                "tohtml",
-                "man",
+        ui = {
+            icons = {
+                lazy = "鈴",
             },
         },
-    },
-})
+        diff = {
+            cmd = "git",
+        },
+        performance = {
+            rtp = {
+                paths = {
+                    vim.fn.expand("~") .. "/.config/nvim",
+                },
+                disabled_plugins = {
+                    "loaded_python3_provider",
+                    "python_provider",
+                    "node_provider",
+                    "ruby_provider",
+                    "perl_provider",
+                    "2html_plugin",
+                    "getscript",
+                    "getscriptPlugin",
+                    "gzip",
+                    "tar",
+                    "tarPlugin",
+                    "rrhelper",
+                    "vimball",
+                    "vimballPlugin",
+                    "zip",
+                    "zipPlugin",
+                    "tutor",
+                    "rplugin",
+                    "logiPat",
+                    "netrwSettings",
+                    "netrwFileHandlers",
+                    "syntax",
+                    "synmenu",
+                    "optwin",
+                    "compiler",
+                    "bugreport",
+                    "ftplugin",
+                    "load_ftplugin",
+                    "indent_on",
+                    "netrwPlugin",
+                    "tohtml",
+                    "man",
+                },
+            },
+        },
+    }, require("omega.custom.config.lazy"))
+)
