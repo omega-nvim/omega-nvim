0918a43c25d4f54a0350c5890d1dd15275ec8b57
Max <max.heinzer397@gmx.ch>
2023-02-17 14:30:19 +0100

    fix lazy setup

--- lua/omega/core/lazy.lua
+++ lua/omega/core/lazy.lua
@@ -11,68 +11,69 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
-require("lazy").setup({
-    spec = {
-        { import = "omega.modules" },
-        { import = "omega.custom.modules" },
+local settings = vim.tbl_deep_extend("force", {
+    defaults = {
+        lazy = true,
     },
-    vim.tbl_deep_extend("force", {
-        defaults = {
-            lazy = true,
+    ui = {
+        custom_keys = {
+            ["<leader>?"] = function(plugin)
+                vim.pretty_print(plugin)
+            end,
         },
-        ui = {
-            custom_keys = {
-                ["<leader>?"] = function(plugin)
-                    vim.pretty_print(plugin)
-                end,
+        icons = {
+            lazy = "鈴",
+        },
+    },
+    diff = {
+        cmd = "git",
+    },
+    performance = {
+        rtp = {
+            paths = {
+                vim.fn.expand("~") .. "/.config/nvim",
             },
-            icons = {
-                lazy = "鈴",
-            },
-        },
-        diff = {
-            cmd = "git",
-        },
-        performance = {
-            rtp = {
-                paths = {
-                    vim.fn.expand("~") .. "/.config/nvim",
-                },
-                disabled_plugins = {
-                    "loaded_python3_provider",
-                    "python_provider",
-                    "node_provider",
-                    "ruby_provider",
-                    "perl_provider",
-                    "2html_plugin",
-                    "getscript",
-                    "getscriptPlugin",
-                    "gzip",
-                    "tar",
-                    "tarPlugin",
-                    "rrhelper",
-                    "vimball",
-                    "vimballPlugin",
-                    "zip",
-                    "zipPlugin",
-                    "tutor",
-                    "rplugin",
-                    "logiPat",
-                    "netrwSettings",
-                    "netrwFileHandlers",
-                    "syntax",
-                    "synmenu",
-                    "optwin",
-                    "compiler",
-                    "bugreport",
-                    "ftplugin",
-                    "load_ftplugin",
-                    "indent_on",
-                    "netrwPlugin",
-                    "tohtml",
-                    "man",
-                },
+            disabled_plugins = {
+                "loaded_python3_provider",
+                "python_provider",
+                "node_provider",
+                "ruby_provider",
+                "perl_provider",
+                "2html_plugin",
+                "getscript",
+                "getscriptPlugin",
+                "gzip",
+                "tar",
+                "tarPlugin",
+                "rrhelper",
+                "vimball",
+                "vimballPlugin",
+                "zip",
+                "zipPlugin",
+                "tutor",
+                "rplugin",
+                "logiPat",
+                "netrwSettings",
+                "netrwFileHandlers",
+                "syntax",
+                "synmenu",
+                "optwin",
+                "compiler",
+                "bugreport",
+                "ftplugin",
+                "load_ftplugin",
+                "indent_on",
+                "netrwPlugin",
+                "tohtml",
+                "man",
             },
         },
-    }, require("omega.custom.config.lazy")),
-})
+    },
+}, require("omega.custom.config.lazy"))
+
+settings.spec = {
+    { import = "omega.modules" },
+    { import = "omega.custom.modules" },
+}
+
+require("lazy").setup(settings)
