c2960773d0fb5facfd3c12e6979f8a06c21979e8
Max <max.heinzer397@gmx.ch>
2023-02-20 11:42:29 +0100

    feat(queries/norg): use hack to readd todo item highlights

---
+++ queries/norg/highlights.scm
@@ -0,0 +1,10 @@
+; TODO: eventually remove this
+;; extends
+(todo_item_undone) @neorg.todo_items.undone.1
+(todo_item_done) @neorg.todo_items.done.1
+(todo_item_pending) @neorg.todo_items.pending.1
+(todo_item_on_hold) @neorg.todo_items.on_hold.1
+(todo_item_cancelled) @neorg.todo_items.cancelled.1
+(todo_item_uncertain) @neorg.todo_items.uncertain.1
+(todo_item_urgent) @neorg.todo_items.urgent.1
+(todo_item_recurring) @neorg.todo_items.recurring.1
