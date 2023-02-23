042b8c1532325e333353844722f12a26204dc392
Max <max.heinzer397@gmx.ch>
2023-02-23 17:00:44 +0100

    fix(langs): fix rust analyzer settings

--- lua/omega/modules/langs/rust.lua
+++ lua/omega/modules/langs/rust.lua
@@ -2,22 +2,20 @@ local function on_attach(client, bufnr)
     require("omega.modules.lsp.on_attach").setup(client, bufnr)
 end
 require("lspconfig")["rust_analyzer"].setup({
-    rust_analyzer = {
-        on_attach = on_attach,
-        standalone = true,
-        settings = {
-            ["rust-analyzer"] = {
-                editor = {
-                    formatOnType = true,
-                },
-                checkOnSave = {
-                    command = "clippy",
-                },
-                hover = {
-                    actions = {
-                        references = {
-                            enable = true,
-                        },
+    on_attach = on_attach,
+    standalone = true,
+    settings = {
+        ["rust-analyzer"] = {
+            editor = {
+                formatOnType = true,
+            },
+            checkOnSave = {
+                command = "clippy",
+            },
+            hover = {
+                actions = {
+                    references = {
+                        enable = true,
                     },
                 },
             },
