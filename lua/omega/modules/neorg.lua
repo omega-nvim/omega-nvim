eece826bdfe65bdb5a262b9567f7c2a9d2cc6740
Max <max.heinzer397@gmx.ch>
2023-03-01 09:48:12 +0100

    feat(neorg): defer loading of cmp

--- lua/omega/modules/neorg.lua
+++ lua/omega/modules/neorg.lua
@@ -49,18 +49,20 @@ local neorg_mod = {
 
 function neorg_mod.config(_, opts)
     require("neorg").setup(opts)
-    local ok, cmp = pcall(require, "cmp")
-    if ok then
-        local cmp_opts =
-            require("lazy.core.plugin").values(require("lazy.core.config").plugins["nvim-cmp"], "opts", false)
-        local mod_ok = pcall(neorg.modules.load_module, "core.norg.completion", nil, {
-            engine = "nvim-cmp",
-        })
-        if mod_ok then
-            table.insert(cmp_opts.sources, { name = "neorg", priority = 6 })
+    vim.defer_fn(function()
+        local ok, cmp = pcall(require, "cmp")
+        if ok then
+            local cmp_opts =
+                require("lazy.core.plugin").values(require("lazy.core.config").plugins["nvim-cmp"], "opts", false)
+            local mod_ok = pcall(neorg.modules.load_module, "core.norg.completion", nil, {
+                engine = "nvim-cmp",
+            })
+            if mod_ok then
+                table.insert(cmp_opts.sources, { name = "neorg", priority = 6 })
+            end
+            cmp.setup(cmp_opts)
         end
-        cmp.setup(cmp_opts)
-    end
+    end, 1)
 end
 
 return neorg_mod
