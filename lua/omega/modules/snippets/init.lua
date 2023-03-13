ff96955e4c66e6ee472eaf14892128aa2695f43b
Max <max.heinzer397@gmx.ch>
2023-03-13 19:11:48 +0100

    feat: better luasnip config

--- lua/omega/modules/snippets/init.lua
+++ lua/omega/modules/snippets/init.lua
@@ -1,10 +1,30 @@
-local snippets = { "L3MON4D3/LuaSnip", build = "make install_jsregexp", event = "InsertEnter" }
+local snippets = {
+    "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
+    event = "InsertEnter",
+    opts = function()
+        return {
+            update_events = { "TextChanged", "TextChangedI" },
+            region_check_events = "InsertEnter",
 
-function snippets.config()
-    require("luasnip").setup({
-        update_events = { "TextChanged", "TextChangedI" },
-        region_check_events = "InsertEnter",
-    })
+            ext_opts = {
+                [require("luasnip.util.types").choiceNode] = {
+                    active = {
+                        virt_text = { { " ", "Keyword" } },
+                    },
+                },
+                [require("luasnip.util.types").insertNode] = {
+                    active = {
+                        virt_text = { { "●", "Special" } },
+                    },
+                },
+            },
+        }
+    end,
+}
+
+function snippets.config(_, opts)
+    require("luasnip").setup(opts)
     require("omega.modules.snippets.lua")
     require("omega.modules.snippets.tex")
 end
