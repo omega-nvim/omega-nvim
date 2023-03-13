0eb66b77a15809ee85b889257142137179cdb0e6
Max <max.heinzer397@gmx.ch>
2023-03-13 18:01:51 +0100

    feat(types): added ayu_light colorscheme

--- lua/omega/types.lua
+++ lua/omega/types.lua
@@ -2,6 +2,6 @@
 
 ---@alias CmdlinePosition "normal"|"top"|"bottom"
 
----@alias OmegaColorscheme "onedark"|"tokyonight_storm"|"doom_one"|"kanagawa"|"everforest"
+---@alias OmegaColorscheme "onedark"|"tokyonight_storm"|"doom_one"|"kanagawa"|"everforest"|"ayu_light"
 
 ---@alias TablineSeparatorStyle "thin"|"padding"|"angled"
