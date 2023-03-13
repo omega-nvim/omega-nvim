43c7c8a27790bd8d96a0190c7eb5f100418d9ff7
Max <max.heinzer397@gmx.ch>
2023-03-13 12:03:48 +0100

    fix colorschemes with light bg

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-vim.g.colors_name = require("omega.custom.config").ui.colorscheme
 require("omega.core")
 local colorscheme_path = vim.fn.stdpath("cache") .. "/omega/highlights"
 if not vim.loop.fs_stat(colorscheme_path) then
