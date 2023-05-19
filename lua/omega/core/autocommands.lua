7a8c637d33abc9c86c3a8913fd6f1913f71ebe81
Max <max.heinzer397@gmx.ch>
2023-05-19 10:07:41 +0200

    fix(telescope): add workaround for bug

--- lua/omega/core/autocommands.lua
+++ lua/omega/core/autocommands.lua
@@ -172,3 +172,13 @@ autocmd("OptionSet", {
         end
     end,
 })
+
+-- TODO: remove when fixed
+-- Temporay workaround for bug: https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1510001730
+vim.api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+        if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+        end
+    end,
+})
