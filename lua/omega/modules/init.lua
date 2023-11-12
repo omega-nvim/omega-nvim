7d7d70b4d6d2e5707b162a95f2c3c9e77dbd1153
Max <max.heinzer397@gmx.ch>
2023-11-12 16:14:00 +0100

    fix loading of things

--- lua/omega/modules/init.lua
+++ lua/omega/modules/init.lua
@@ -1,9 +1,23 @@
+require("omega.config").init()
+
 return {
+    { "omega-nvim/omega-nvim", config = true, lazy = false, priority = 10000, cond = true },
     { "nvim-tree/nvim-web-devicons" },
     { "rcarriga/nvim-notify" },
     { "MunifTanjim/nui.nvim" },
     { "nvim-lua/plenary.nvim" },
-    { "max397574/omega-themes", lazy = false },
+    {
+        "max397574/omega-themes",
+        lazy = false,
+        priority = 100,
+        config = function()
+            local colorscheme_path = vim.fn.stdpath("cache") .. "/omega/highlights"
+            if not vim.loop.fs_stat(colorscheme_path) then
+                require("omega.colors").compile_theme(require("omega.config").colorscheme)
+            end
+            loadfile(colorscheme_path)()
+        end,
+    },
     {
         "glepnir/nerdicons.nvim",
         cmd = "NerdIcons",
